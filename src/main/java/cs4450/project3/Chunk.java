51d433c1bfe916ff62199e0668c372fa851d3838
Arze Lu <arze2014@gmail.com>
2023-10-30 22:15:34 -0700

    moa

--- src/main/java/cs4450/project3/Chunk.java
+++ src/main/java/cs4450/project3/Chunk.java
@@ -20,32 +20,94 @@ public class Chunk {
     private Block[][][] blocks;
     private int VBOVertexHandle;
     private int VBOColorHandle;
+    private int VBOTextureHandle;
+    private Texture texture;
     private int startX, startY, startZ;
     private Random r;
+    
+    public void render(){
+        glPushMatrix();
+        glBindBuffer(GL_ARRAY_BUFFER, VBOVertexHandle);
+        glBindTexture(GL_TEXTURE_2D, 1);
+        glTexCoordPointer(2, GL_FLOAT, 0, 0L);
+        glVertexPointer(3, GL_FLOAT, 0, 0L);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOColorHandle);
+        glColorPointer(3, GL_FLOAT, 0, 0L);
+        glDrawArrays(GL_QUADS, 0, CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE * 24);
+        glPopMatrix();
+    }
+    
+    public static float[] createTexCube(float x, float y, Block block){
+        float offset = (1024f / 16) / 1024f;
+        
+        switch(block.getID()){
+            case 1:
+                return new float[]{
+                    // BOTTOM QUAD(DOWN=+Y)
+                    x + offset*3, y + offset*10,
+                    x + offset*2, y + offset*10,
+                    x + offset*2, y + offset*9,
+                    x + offset*3, y + offset*9,
+                    // TOP!
+                    x + offset*3, y + offset*1,
+                    x + offset*2, y + offset*1,
+                    x + offset*2, y + offset*0,
+                    x + offset*3, y + offset*0,
+                    // FRONT QUAD
+                    x + offset*3, y + offset*0,
+                    x + offset*4, y + offset*0,
+                    x + offset*4, y + offset*1,
+                    x + offset*3, y + offset*1,
+                    // BACK QUAD
+                    x + offset*4, y + offset*1,
+                    x + offset*3, y + offset*1,
+                    x + offset*3, y + offset*0,
+                    x + offset*4, y + offset*0,
+                    // LEFT QUAD
+                    x + offset*3, y + offset*0,
+                    x + offset*4, y + offset*0,
+                    x + offset*4, y + offset*1,
+                    x + offset*3, y + offset*1,
+                    // RIGHT QUAD
+                    x + offset*3, y + offset*0,
+                    x + offset*4, y + offset*0,
+                    x + offset*4, y + offset*1,
+                    x + offset*3, y + offset*1
+                };
+        }
+    }
     
     public void rebuildMesh(float startX, float startY, float startZ){
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
+        VBOTextureHandle = glGenBuffers();
+        FloatBuffer vertexTextureData = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE) * 6 * 12);
         FloatBuffer vertexPositionData = BufferUtils.createFloatBuffer((int) (Math.pow(CHUNK_SIZE, 3.0f) * 72));
         FloatBuffer vertexColorData = BufferUtils.createFloatBuffer((int) (Math.pow(CHUNK_SIZE, 3.0f) * 72));
         
         for(float x = 0; x < CHUNK_SIZE; x++){
             for(float z = 0; z < CHUNK_SIZE; z++){
                 for(float y = 0; y < CHUNK_SIZE; y++){
+                    vertexTextureData.put(createTexCube((float)0, (float)0, blocks[(int)x][(int)y][(int)z]));
                     vertexPositionData.put(createCube((float)(startX + x * CUBE_LENGTH), (float)(y * CUBE_LENGTH + (int)(CHUNK_SIZE * 0.8)), (float)(startZ + z * CUBE_LENGTH)));
                     vertexColorData.put(createCubeVertexCol(getCubeColor(blocks[(int)x][(int)y][(int)z])));
-                    
                 }
             }
         }
         
+        vertexTextureData.flip();
         vertexColorData.flip();
         vertexPositionData.flip();
         glBindBuffer(GL_ARRAY_BUFFER, VBOVertexHandle);
         glBufferData(GL_ARRAY_BUFFER, vertexPositionData, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
+        
         glBindBuffer(GL_ARRAY_BUFFER, VBOColorHandle);
         glBufferData(GL_ARRAY_BUFFER, vertexColorData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        
+        glBindBuffer(GL_ARRAY_BUFFER, VBOTextureHandle);
+        glBufferData(GL_ARRAY_BUFFER, vertexTextureData, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
     
@@ -94,18 +156,24 @@ public class Chunk {
     }
     
     private float[] getCubeColor(Block block){
-        switch(block.getID()){
-            case 1:
-                return new float[]{0, 1, 0};
-            case 2:
-                return new float[]{1, 0.5f, 0};
-            case 3:
-                return new float[]{0, 0f, 1f};
-        }
+//        switch(block.getID()){
+//            case 1:
+//                return new float[]{0, 1, 0};
+//            case 2:
+//                return new float[]{1, 0.5f, 0};
+//            case 3:
+//                return new float[]{0, 0f, 1f};
+//        }
         return new float[]{1, 1, 1};
     }
     
     public Chunk(int startX, int startY, int startZ){
+        try{
+            texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("terrain.png"));
+        }catch(Exception e){
+            System.out.println("Exception in Chunk constructor");
+        }
+        
         r = new Random();
         blocks = new Block[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
         for (int x = 0; x < CHUNK_SIZE; x++) {
@@ -129,6 +197,7 @@ public class Chunk {
         }
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
+        VBOTextureHandle = glGenBuffers();
         this.startX = startX;
         this.startY = startY;
         this.startZ = startZ;
